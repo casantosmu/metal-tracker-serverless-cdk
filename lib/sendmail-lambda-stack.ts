5a33bbd9e55a9c91f3beef6fed79353e38a44540
Carlos Santos <casantosmu@gmail.com>
2023-08-07 06:39:21 +0200

    Add sendmail topic and logic on sendmail lambda

--- lib/sendmail-lambda-stack.ts
+++ lib/sendmail-lambda-stack.ts
@@ -5,9 +5,31 @@ import {
   aws_dynamodb as dynamodb,
   aws_logs as logs,
   aws_lambda_nodejs as nodejs,
+  aws_sns as sns,
+  aws_sns_subscriptions as subscriptions,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import path from "path";
+
+const getEmailAddressEnv = () => {
+  const emailAddressEnv = process.env.SEND_EMAIL_ADDRESS;
+
+  if (emailAddressEnv === undefined) {
+    throw Error("'SEND_EMAIL_ADDRESS' env must be provided");
+  }
+
+  return emailAddressEnv;
+};
+
+const getSendMailTopicArnEnv = () => {
+  const sendMailTopicArnEnv = process.env.SEND_MAIL_TOPIC_ARN;
+
+  if (sendMailTopicArnEnv === undefined) {
+    throw Error("'SEND_MAIL_TOPIC_ARN' env must be provided");
+  }
+
+  return sendMailTopicArnEnv;
+};
 
 type SendMailLambdaStackProps = {
   table: dynamodb.Table;
@@ -17,13 +39,16 @@ export class SendMailLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: SendMailLambdaStackProps) {
     super(scope, id, props);
 
-    const fn = new nodejs.NodejsFunction(this, "send-email-lambda", {
+    const lambdaFn = new nodejs.NodejsFunction(this, "send-email-lambda", {
       entry: path.join("src", "lambda", "sendmail-lambda.ts"),
       runtime: lambda.Runtime.NODEJS_18_X,
       logRetention: logs.RetentionDays.TWO_WEEKS,
+      environment: {
+        SEND_MAIL_TOPIC_ARN: getSendMailTopicArnEnv(),
+      },
     });
 
-    fn.addEventSource(
+    lambdaFn.addEventSource(
       new eventsources.DynamoEventSource(props.table, {
         startingPosition: lambda.StartingPosition.LATEST,
         filters: [
@@ -33,5 +58,15 @@ export class SendMailLambdaStack extends cdk.Stack {
         ],
       })
     );
+
+    const topic = new sns.Topic(this, "send-email-topic", {
+      topicName: "metal-tracker-send-email-topic",
+    });
+
+    topic.addSubscription(
+      new subscriptions.EmailSubscription(getEmailAddressEnv())
+    );
+
+    topic.grantPublish(lambdaFn);
   }
 }
--- src/lambda/sendmail-lambda.ts
+++ src/lambda/sendmail-lambda.ts
@@ -1,11 +1,56 @@
 import { Handler, DynamoDBStreamHandler } from "aws-lambda";
 import { lambdaRequestTracker } from "pino-lambda";
 import { logger } from "../utils/logger/logger";
+import AWS from "aws-sdk";
+import { z } from "zod";
+
+const sns = new AWS.SNS();
+
+const metalTrackerTableEventRecordSchema = z.object({
+  dynamodb: z.object({
+    NewImage: z.object({
+      date: z.object({ S: z.string() }),
+      link: z.object({ S: z.string() }),
+      title: z.object({ S: z.string() }),
+      summary: z.object({ S: z.string() }),
+    }),
+    Keys: z.object({
+      PK: z.object({
+        S: z.string(),
+      }),
+    }),
+  }),
+});
 
 const withRequest = lambdaRequestTracker();
 
 export const handler: DynamoDBStreamHandler = async (event, context) => {
   withRequest(event, context);
 
-  logger.info(event, "events :>> ");
+  const snsPromises = event.Records.map(async (record) => {
+    const data = metalTrackerTableEventRecordSchema.parse(record);
+
+    const {
+      date: { S: date },
+      link: { S: link },
+      summary: { S: summary },
+      title: { S: title },
+    } = data.dynamodb.NewImage;
+
+    const blogName = data.dynamodb.Keys.PK.S;
+
+    const emailSubject = "Metal Tracker - New album review: " + title;
+    const emailMessage = `A new album review has been published on ${blogName} \n\nTitle: ${title}\nDate: ${date}\nSummary: ${summary}\nLink: ${link}`;
+
+    const params = {
+      Subject: emailSubject,
+      Message: emailMessage,
+      TopicArn: process.env.SEND_MAIL_TOPIC_ARN,
+    };
+
+    const result = await sns.publish(params).promise();
+    logger.info({ result }, "Message sent successfully");
+  });
+
+  await Promise.all(snsPromises);
 };
